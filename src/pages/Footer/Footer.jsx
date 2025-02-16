0e8bed1c5300a8bfd60f3c493d4236eac9d881bc
mariam titvinidze <titvinidzemariam3@gmail.com>
2025-02-16 16:58:37 +0400

    1

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -7,65 +7,65 @@ const Footer = () => {
   const location=useLocation()
   const data=location.state
 
-  const [FirstBTN,setFirstBTN]=useState(true)
-  const [SecondBTN,setSEcondBTN]=useState(false)
-  const [ThirdBTN,setThirdBTN]=useState(false)
+  const [Buttonone,setButtonone]=useState(true)
+  const [Buttontwo,setButtontwo]=useState(false)
+  const [Buttonthree,setButtonthree]=useState(false)
 
-  const FirstBTNHandle = (e) =>{
-    setFirstBTN(true)
-    setSEcondBTN(false)
-    setThirdBTN(false)
+  const ButtononeHandle = (e) =>{
+    setButtonone(true)
+    setButtontwo(false)
+    setButtonthree(false)
   }
 
-  const SecondBTNHandle = (e) =>{
-    setSEcondBTN(true)
-    setFirstBTN(false)
-    setThirdBTN(false)
+  const ButtontwoHandle = (e) =>{
+    setButtontwo(true)
+    setButtonone(false)
+    setButtonthree(false)
   }
 
-  const ThirdBTNHandle = (e) =>{
-    setSEcondBTN(false)
-    setFirstBTN(false)
-    setThirdBTN(true)
+  const ButtonthreeHandle = (e) =>{
+    setButtontwo(false)
+    setButtonone(false)
+    setButtonthree(true)
   }
 
 
 return (
   <>
   <div className='w-full h-[50px] justify-center gap-[30px] border-b border-b-[#FFFFFF]  hidden max-md:flex'>
-    <button className={`text-white ${FirstBTN ? "opacity" : "opacity-[20%]" } ${FirstBTN && "border-b-[#419EBB] && border-b-[4px]"} font-[spartan] text-[9px] font-[700] `} onClick={(e)=> FirstBTNHandle(e)}>OVERVIEW</button>
-    <button className={`text-white ${SecondBTN ? "opacity" : "opacity-[20%]" } ${SecondBTN && "border-b-[#419EBB] && border-b-[4px]"} font-[spartan] text-[9px] font-[700]`} onClick={(e)=> SecondBTNHandle(e)}>Structure</button>
-    <button className={`text-white ${ThirdBTN ? "opacity" : "opacity-[20%]" } ${ThirdBTN && "border-b-[#419EBB] && border-b-[4px]"}font-[spartan]  text-[9px] font-[700]`} onClick={(e)=> ThirdBTNHandle(e)}>Surface </button>
+    <button className={`text-white ${Buttonone ? "opacity" : "opacity-[20%]" } ${Buttonone && "border-b-[#419EBB] && border-b-[4px]"} font-[spartan] text-[9px] font-[700] `} onClick={(e)=> ButtononeHandle(e)}>OVERVIEW</button>
+    <button className={`text-white ${Buttontwo ? "opacity" : "opacity-[20%]" } ${Buttontwo && "border-b-[#419EBB] && border-b-[4px]"} font-[spartan] text-[9px] font-[700]`} onClick={(e)=> ButtontwoHandle(e)}>Structure</button>
+    <button className={`text-white ${Buttonthree ? "opacity" : "opacity-[20%]" } ${Buttonthree && "border-b-[#419EBB] && border-b-[4px]"}font-[spartan]  text-[9px] font-[700]`} onClick={(e)=> ButtonthreeHandle(e)}>Surface </button>
   </div>
   <div className='flex h-[80vh] max-xl:flex-col max-xl:h-[135vh] max-xl:items-center max-md:h-[68vh]'>
 
-    <div className={`w-[60%] flex justify-center items-center ${ThirdBTN && "relative"} max-xl:pt-[100px]`}>
-      {FirstBTN && <img className=' max-md:w-[224px] max-md:h-[224px]' src={data.images.planet} alt="" />}
-      {SecondBTN && <img className=' max-md:w-[224px] max-md:h-[224px]' src={data.images.internal} alt="" />}
-      {ThirdBTN && <><img className=' max-md:w-[224px] max-md:h-[224px]' src={data.images.planet} alt="" /> <img className='w-[163px] h-[199px] absolute bottom-[60px] max-xl:bottom-[-50px] max-md:w-[100px] max-md:h-[120px]' src={data.images.geology} alt="" /></>}
+    <div className={`w-[60%] flex justify-center items-center ${Buttonthree && "relative"} max-xl:pt-[100px]`}>
+      {Buttonone && <img className=' max-md:w-[224px] max-md:h-[224px]' src={data.images.planet} alt="" />}
+      {Buttontwo && <img className=' max-md:w-[224px] max-md:h-[224px]' src={data.images.internal} alt="" />}
+      {Buttonthree && <><img className=' max-md:w-[224px] max-md:h-[224px]' src={data.images.planet} alt="" /> <img className='w-[163px] h-[199px] absolute bottom-[60px] max-xl:bottom-[-50px] max-md:w-[100px] max-md:h-[120px]' src={data.images.geology} alt="" /></>}
     </div>
 
     <div className='w-[40%] mt-[50px] max-xl:flex max-xl:items-center max-xl:justify-center max-xl:gap-[100px] max-md:flex-col'>
       <div className='max-md:flex max-md:flex-col max-md:items-center max-md:w-[327px]'>
         <p className='font-[Antonio] font-normal text-[80px] mb-[20px] text-white max-xl:text-[48px]'>{data.name}</p>
-        <p className='w-[350px] font-[Spartan] font-normal text-[14px] mb-[20px] text-[white] max-xl:text-[11px] max-xl:w-[339px] max-md:w-[327px] max-md:text-center'>{FirstBTN ? data.overview.content : SecondBTN ? data.structure.content : ThirdBTN ? data.geology.content : "#"}</p>
+        <p className='w-[350px] font-[Spartan] font-normal text-[14px] mb-[20px] text-[white] max-xl:text-[11px] max-xl:w-[339px] max-md:w-[327px] max-md:text-center'>{Buttonone ? data.overview.content : Buttontwo ? data.structure.content : Buttonthree ? data.geology.content : "#"}</p>
         <div className='flex items-center gap-[8px] mb-[30px]'>
-        <p className="font-[Spartan] font-normal text-[14px] tracking-[1px] text-[rgba(255,255,255,0.3)] max-xl:text-[12px]">Source: <a className="font-[700] underline text-[#ffffff]" target="_blank" href={FirstBTN ? data.overview.source : SecondBTN ? data.structure.source : ThirdBTN ? data.geology.source : "#"}>Wikipedia</a></p>
+        <p className="font-[Spartan] font-normal text-[14px] tracking-[1px] text-[rgba(255,255,255,0.3)] max-xl:text-[12px]">Source: <a className="font-[700] underline text-[#ffffff]" target="_blank" href={Buttonone ? data.overview.source : Buttontwo ? data.structure.source : ThirdBTN ? data.geology.source : "#"}>Wikipedia</a></p>
         <img className='' src={Picture} alt="" /> 
         </div>
       </div>
       <div className='flex  flex-col gap-[16px] max-md:hidden'>
-      <button onClick={(e)=> FirstBTNHandle(e)} className={`flex gap-[20px] h-[48px] w-[350px] cursor-pointer ${!FirstBTN ? "bg-[#070724] BTN" : "bg-[#419EBB]"} items-center texts max-xl:w-[281px]`}>
+      <button onClick={(e)=> ButtononeHandle(e)} className={`flex gap-[20px] h-[48px] w-[350px] cursor-pointer ${!Buttonone ? "bg-[#070724] BTN" : "bg-[#419EBB]"} items-center texts max-xl:w-[281px]`}>
         <p className='font-[Spartan] font-normal text-[12px] text-white opacity-[50%] ml-[25px] max-xl:text-[9px]'>01</p>
         <p className='font-[Spartan] font-normal text-[12px] text-white ml-[10px] max-xl:text-[9px]'>OVERVIEW</p>
       </button>
 
-      <button onClick={(e)=> SecondBTNHandle(e)} className={`flex gap-[20px] h-[48px] w-[350px] cursor-pointer  ${!SecondBTN ? "bg-[#070724] BTN" : "bg-[#419EBB]"} items-center  texts max-xl:w-[281px]`}>
+      <button onClick={(e)=> ButtontwoHandle(e)} className={`flex gap-[20px] h-[48px] w-[350px] cursor-pointer  ${!Buttontwo ? "bg-[#070724] BTN" : "bg-[#419EBB]"} items-center  texts max-xl:w-[281px]`}>
         <p className='font-[Spartan] font-normal text-[12px] text-white opacity-[50%] ml-[25px] max-xl:text-[9px]'>02</p>
         <p className='font-[Spartan] font-normal text-[12px] text-white ml-[10px] max-xl:text-[9px]'>Internal Structure</p>
       </button>
 
-      <button onClick={(e)=> ThirdBTNHandle(e)} className={`flex gap-[20px] h-[48px] w-[350px] cursor-pointer  ${!ThirdBTN ? "bg-[#070724] BTN" : "bg-[#419EBB]"} items-center  texts max-xl:w-[281px]`}>
+      <button onClick={(e)=> ButtonthreeHandle(e)} className={`flex gap-[20px] h-[48px] w-[350px] cursor-pointer  ${!Buttonthree ? "bg-[#070724] BTN" : "bg-[#419EBB]"} items-center  texts max-xl:w-[281px]`}>
         <p className='font-[Spartan] font-normal text-[12px] text-white opacity-[50%] ml-[25px] max-xl:text-[9px]'>03</p>
         <p className='font-[Spartan] font-normal text-[12px] text-white  ml-[10px] max-xl:text-[9px]'>Surface Geology</p>
       </button>
